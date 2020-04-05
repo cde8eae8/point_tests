ba889515e2144e50cdb4cf875f03e19be8b73a6a
cde8eae8 <nikitadrozd@outlook.com>
2020-04-05 21:25:43 +0300

    tests added

---
+++ test.py
@@ -0,0 +1,16 @@
+import sys
+
+def dist(a, b):
+    return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
+
+n = int(input())
+points = []
+for i in range(n):
+    points.append([int(i) for i in input().split()])
+
+ans = 1000000000000000000
+for i in range(len(points)):
+    for j in range(len(points)):
+        if (i == j): continue
+        ans = min(ans, dist(points[i], points[j]))
+print(ans)
